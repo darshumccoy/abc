d4e816594f51293df1393da59fb45acf0bd66c5b
sunil <sunil@SunilMudaliarUG>
2018-06-06 15:20:27 +0530

    sunil

---
+++ spring-boot-micro-currency-service/src/main/java/com/example/springbootmicrocurrencyservice/SpringBootMicroCurrencyServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.springbootmicrocurrencyservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMicroCurrencyServiceApplicationTest {
+	
+	@Test
+	  public void contextLoads() {
+	  }
+
+}
